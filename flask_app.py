0d519b40d9b87d41eedeb0aa5c41a6912ab3084e
msaaad <saad99u@gmail.com>
2020-01-12 13:02:26 +0500

    updating elements

--- flask_app.py
+++ flask_app.py
@@ -5,11 +5,6 @@ import os
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-#filenumber = int(os.listdir('saved_conversations')[-1])
-#filenumber = filenumber + 1
-#file = open('saved_conversations/'+str(filenumber), "w+")
-#file.close()
-
 app = Flask(__name__)
 
 docbot = ChatBot('Bot',
@@ -33,12 +28,6 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')  #user query
     response = str(docbot.get_response(userText)) #bot response
-
- #   appendfile = os.listdir('saved_conversations')[-1]
-  #  appendfile = open('saved_conversations/'+str(filenumber), "a")
- #   appendfile.write('user : '+userText+'\n')
-  #  appendfile.write('bot : '+response+'\n')
-   # appendfile.close()
 
     return response
 
